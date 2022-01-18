069bb594fbe8558c3995adf5872d0e4a1dd45ef1
AJ <ancheetah@users.noreply.github.com>
2022-01-18 14:39:57 -0500

    config plugin with several page templates

---
+++ src/templates/TestPage.jsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { graphql } from 'gatsby';
+import { BuilderComponent, BuilderContent } from '@builder.io/react';
+import { Helmet } from 'react-helmet';
+import Link from '../components/Link/Link';
+
+const defaultDescription = 'Edit this in your entry for a better SEO';
+
+const defaultTitle = 'Builder: Drag and Drop Page Building for Any Site';
+
+function TestPageTemplate({ data }) {
+  const models = data?.allBuilderModels;
+  const testPage = models.testPage[0]?.content;
+
+  return (
+    <>
+      <Helmet>
+        <title>{(testPage && testPage.data.title) || defaultTitle}</title>
+        <meta
+          name="description"
+          content={
+            (testPage && testPage.data.description) || defaultDescription
+          }
+        />
+      </Helmet>
+      {/** name of the model is test page, change it if you decided to build*/}
+      <BuilderContent content={testPage} model="test-page">
+        {(data) => {
+          console.log("my data: ", data, "my content: ", testPage);
+          return(
+            <BuilderComponent
+              renderLink={Link}
+              name="test-page"
+              content={testPage}
+            />
+          );
+        }}
+      </BuilderContent>
+    </>
+  );
+}
+
+export default TestPageTemplate;
+
+export const testPageQuery = graphql`
+  query($path: String!) {
+    allBuilderModels {
+      testPage(
+        target: { urlPath: $path }
+        limit: 1
+      ) {
+        content
+      }
+    }
+  }
+`;
