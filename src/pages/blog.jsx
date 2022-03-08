b2c3ee41c9a6695469c934e7153fba26a30e3b8c
AJ <ancheetah@users.noreply.github.com>
2022-03-08 17:15:20 -0500

    created main blog page

---
+++ src/pages/blog.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BlogPage = () => (
+    <div>
+        <h1>Welcome to my Blog</h1>
+        <h3>List of Articles</h3>
+        {/* Link to articles here */}
+    </div>
+)
+
+export default BlogPage
+
+// Query articles from builder here to get paths
