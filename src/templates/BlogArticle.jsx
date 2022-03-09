f7c67fbe640aad14c364c5129fcaba253efd1840
AJ <ancheetah@users.noreply.github.com>
2022-03-09 11:36:32 -0500

    move custom component to BlogArticle template

--- src/templates/BlogArticle.jsx
+++ src/templates/BlogArticle.jsx
@@ -1,16 +1,40 @@
-import * as React from 'react';
+import React from 'react';
 import { graphql } from 'gatsby';
-import { BuilderComponent } from '@builder.io/react';
+import { BuilderComponent, Builder, Image } from '@builder.io/react';
 import { Helmet } from 'react-helmet';
 import Link from '../components/Link/Link';
-import '../components/BlogHeader/BlogHeader.builder';
 
 const defaultDescription = 'This is a blog article';
-
 const defaultTitle = 'Blog Article Title';
 
 function BlogArticleTemplate({ data }) {
-  const article = data?.allBuilderModels.blogArticlePage[0]?.content;
+  const blogData = data?.allBuilderModels.blogArticlePage[0];
+  const article = blogData?.content;
+  const blogFields = blogData?.data;
+  
+  const BlogHeader = props => {
+    return (
+      <div>
+        <h2>{blogFields.title}</h2>
+        <p>By {blogFields.author}</p>
+        <Image image={props.image} />
+      </div>
+    );
+  };
+  
+  Builder.registerComponent(BlogHeader, {
+    name: 'Blog Header',
+    inputs: [
+      {
+        name: 'image',
+        type: 'file',
+        allowedFileTypes: ['jpeg', 'jpg', 'png', 'svg'],
+        required: true,
+        defaultValue:
+          'https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F52dcecf48f9c48cc8ddd8f81fec63236'
+      },
+    ]
+  });
 
   return (
     <>
@@ -44,6 +68,10 @@ export const blogArticleQuery = graphql`
             options: { cachebust: true }
         ) {
             content
+            data {
+              title
+              author
+            }
         }
     }
   }
