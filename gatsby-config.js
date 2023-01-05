7eceb434e8fa2f54590db890c8d6d7ca35147755
AJ <ancheetah@users.noreply.github.com>
2023-01-05 17:34:27 -0500

    override 404 in development

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,8 @@ module.exports = {
           // Render every `landingPage` model as a new page using the 
           // src/templates/LandingPage.jsx template based on the URL provided in Builder.io
           landingPage: path.resolve('src/templates/LandingPage.jsx')
-        }
+        },
+        custom404Dev: path.resolve('./src/pages/404.jsx')
       }
     }
   ]
